d0818a4b136ed881f6d0947cbc26e4bb16a632cf
Ahmed Tarek <pushqrdx@gmail.com>
2024-03-24 11:32:49 +0200

    fix #52

--- client/providers/html.js
+++ client/providers/html.js
@@ -7,8 +7,7 @@ const cache_1 = require("../cache");
 class HTMLCompletionItemProvider {
     constructor() {
         this._htmlLanguageService = vscode_html_languageservice_1.getLanguageService();
-        this._expression = /(\/\*\s*html\s*\*\/\s*`|html\s*`)([^`]*)(`)/gi;
-        // private _expression = /(html\s*`)([^`]*)(`)/g
+        this._expression = /(\/\*\s*html\s*\*\/\s*`|(?<!`)html\s*`)([^`]*)(`)/gi;
         this._cache = new cache_1.CompletionsCache();
     }
     provideCompletionItems(document, position, token) {
